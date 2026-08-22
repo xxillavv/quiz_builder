d7b25060a5bd2fe48268bf25da44c9c9509b9af6
Vladyslav <vladpaniv123@gmail.com>
2026-08-22 17:22:21 +0300

    chore: config cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "http://localhost:3000"
+  })
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
